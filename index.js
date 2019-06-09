ba8044bb9c889137420602e6de8ae500692f40e0
Dmidvor <d.dvorov@yandex.ru>
2019-06-09 23:37:28 +0300

    + pg

--- index.js
+++ index.js
@@ -6,19 +6,15 @@ const app = express();
 app.use(express.static('./public'));
 
 app.get('/', function(req, res){
-    
     res.sendFile(__dirname + '/main.html');
-    
 });
 
 app.get('/script.js', function(req, res){
-    
-    res.sendFile(__dirname + '/script.js')
-    
-})
+    res.sendFile(__dirname + '/script.js');
+});
 
 app.listen(3030, function(){
-    console.log('Server start on port 3030')
-})
+    console.log('Server start on port 3030');
+});
 
 
